9262e18a9fc5de06c4db6fe9238c6cd447a717ec
Archit404Error <4architmehta@gmail.com>
2022-12-24 23:20:08 -0500

    Add crowns, optimizations, and sugar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import sys
 from math import *
 from os import *
 from random import *
+from tkinter import Tk, messagebox
 
 import pygame
 
@@ -15,7 +17,7 @@ class Tile:
         self.height = 10
         self.color = col
         self.value = randint(1, 10)
-        self.pos = [loc[0] * 10, loc[1] * 10]
+        self.pos = (loc[0] * 10, loc[1] * 10)
         self.loc = loc
         self.occupied = False
         self.owner = None
@@ -31,11 +33,14 @@ class Tile:
 
 
 class Kingdom:
-    def __init__(self, startTile):
+    def __init__(self, startTile: Tile):
         self.land = [startTile]
+        self.tile_posns = set([startTile.pos])
+        self.capital_pos = startTile.pos
         self.strength = randint(1, 5)
-        self.pacificity = randint(1, 4)
         self.piety = randint(1, 5)
+        self.belligerence = randint(1, 10)
+        self.pacificity = randint(1, 4)
         self.color = (randint(0, 255), randint(0, 255), randint(0, 255))
         self.wins = 0
         self.battles = 0
@@ -45,11 +50,12 @@ class Kingdom:
         startTile.capital = True
         startTile.owner = self
 
-    def loseTile(self, tile):
+    def loseTile(self, tile: Tile):
         self.strength -= uniform(1.0, 10.0)
+        self.tile_posns.remove(tile.pos)
         self.land.pop(self.land.index(tile))
 
-    def gainTile(self, tile):
+    def gainTile(self, tile: Tile):
         if not tile.capital:
             tile.changeColor(self.color)
             tile.occupied = True
@@ -57,8 +63,9 @@ class Kingdom:
                 self.strength += uniform(1.0, 10.0)
             tile.owner = self
             self.land.append(tile)
+            self.tile_posns.add(tile.pos)
 
-    def tileBattle(self, tile):
+    def tileBattle(self, tile: Tile):
         if tile.occupied and tile.owner != self:
             self.battles += 1
             if (
@@ -72,12 +79,14 @@ class Kingdom:
                 else:
                     tile.capital = False
                     tile.changeColor(self.color)
-                    for i in board:
-                        if i.color == tile.owner.color:
-                            self.gainTile(i)
-                        tile.owner.land = []
-                        tile.owner.strength = 0
-                        tile.owner.alive = False
+                    tile.owner.alive = False
+                    old_owner = tile.owner
+                    for t in tile.owner.land:
+                        self.gainTile(t)
+                    old_owner.land = []
+                    old_owner.tile_posns = set()
+                    old_owner.strength = 0
+                    old_owner.piety = 0
             else:
                 self.strength = floor(0.9 * self.strength)
                 self.piety = floor(0.95 * self.piety)
@@ -146,6 +155,7 @@ class Kingdom:
         if randint(1, 5) <= self.pacificity:
             self.pray()
         else:
+            self.strength += self.belligerence
             self.expand()
 
 
@@ -153,7 +163,50 @@ def top_kings(king_lst):
     return sorted(king_lst, key=lambda k: -1 * k.strength * k.piety)[0:5]
 
 
-def runGame():
+def render_high_scores(king: Kingdom, i, font, screen):
+    screen.blit(font.render("Top Scores", True, (255, 255, 255)), (620, 25))
+    screen.blit(
+        font.render(
+            "{}".format(king.strength * king.piety),
+            True,
+            king.color,
+        ),
+        (620, 50 + 20 * i),
+    )
+
+    screen.blit(font.render("Top Strengths", True, (255, 255, 255)), (620, 175))
+    screen.blit(
+        font.render(
+            "{}".format(king.strength),
+            True,
+            king.color,
+        ),
+        (620, 200 + 20 * i),
+    )
+
+    screen.blit(font.render("Top Piety", True, (255, 255, 255)), (620, 325))
+    screen.blit(
+        font.render(
+            "{}".format(king.piety),
+            True,
+            king.color,
+        ),
+        (620, 350 + 20 * i),
+    )
+
+    screen.blit(font.render("Top Records", True, (255, 255, 255)), (620, 475))
+    screen.blit(
+        font.render(
+            "{}/{}".format(king.wins, king.battles),
+            True,
+            king.color,
+        ),
+        (620, 500 + 20 * i),
+    )
+
+
+def run_game():
+    Tk().wm_withdraw()
     pygame.init()
     pygame.display.set_caption("AI Kingdom Battle")
     screen = pygame.display.set_mode((screenWidth, screenHeight))
@@ -169,60 +222,54 @@ def runGame():
         kingdoms.append(Kingdom(board[randint(0, len(board) - 1)]))
 
     font = pygame.font.SysFont(None, 24)
+
+    crowns = []
+    for i in range(1, 4):
+        temp_crown = pygame.image.load(f"king{i}.png")
+        temp_crown = pygame.transform.scale(temp_crown, (50, 20))
+        crowns.append(temp_crown)
+
+    pygame.display.set_icon(crowns[0])
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                m_x, m_y = pygame.mouse.get_pos()
+                pos = (round(m_x / 10) * 10, round(m_y / 10) * 10)
+                for king in kingdoms:
+                    if king.alive and pos in king.tile_posns:
+                        messagebox.showinfo(
+                            "OK",
+                            f"""
+                            Land Tiles: {len(king.tile_posns)}
+                            Strength: {king.strength}
+                            Piety: {king.piety}
+                            Pacificity: {king.pacificity}
+                            Belligerence: {king.belligerence}
+                            """,
+                        )
+                        break
         clock.tick(60)
+
         for tile in board:
             tile.draw(screen)
+
         for king in kingdoms:
             if king.alive:
                 king.move()
+
         pygame.draw.rect(screen, (0, 0, 0), (600, 0, screenWidth, screenHeight))
         to_rank = top_kings(kingdoms)
         for i, king in enumerate(to_rank):
-            screen.blit(font.render("Top Scores", True, (255, 255, 255)), (620, 25))
-            screen.blit(
-                font.render(
-                    "{}".format(king.strength * king.piety),
-                    True,
-                    king.color,
-                ),
-                (620, 50 + 20 * i),
-            )
-
-            screen.blit(font.render("Top Strengths", True, (255, 255, 255)), (620, 175))
-            screen.blit(
-                font.render(
-                    "{}".format(king.strength),
-                    True,
-                    king.color,
-                ),
-                (620, 200 + 20 * i),
-            )
-
-            screen.blit(font.render("Top Piety", True, (255, 255, 255)), (620, 325))
-            screen.blit(
-                font.render(
-                    "{}".format(king.piety),
-                    True,
-                    king.color,
-                ),
-                (620, 350 + 20 * i),
-            )
-
-            screen.blit(font.render("Top Records", True, (255, 255, 255)), (620, 475))
-            screen.blit(
-                font.render(
-                    "{}/{}".format(king.wins, king.battles),
-                    True,
-                    king.color,
-                ),
-                (620, 500 + 20 * i),
-            )
+            if i < len(crowns):
+                screen.blit(
+                    crowns[i], (king.capital_pos[0] - 20, king.capital_pos[1] - 20)
+                )
+            render_high_scores(king, i, font, screen)
         pygame.display.update()
 
 
 if __name__ == "__main__":
-    runGame()
+    run_game()
